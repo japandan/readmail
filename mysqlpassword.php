6280531842371b49f50882998d2842f6476871f6
japandan <dan.vogel@gmail.com>
2019-07-10 16:12:59 -0400

    Update mysqlpassword.php

--- mysqlpassword.php
+++ mysqlpassword.php
@@ -5,6 +5,8 @@ $password="{mysql password}";
 $username="{mysql username}";
 $servername="localhost" ;
 $dbase="{mysql database name}" ;
+
+$imap_server="{datostech.com:143}";
 $imap_user="{email address for imap}";
 $imap_password="{email password}";
 $tech_emails = "dan.vogel@example.com, support@example.com";
