da731cf592c664a95314b302a0065e4f87494be4
Dan Vogel <dan.vogel@gmail.com>
2019-12-30 10:33:41 -0500

    Update mysqlpassword.php

--- mysqlpassword.php
+++ mysqlpassword.php
@@ -5,6 +5,10 @@ $password="{mysql password}";
 $username="{mysql username}";
 $servername="localhost" ;
 $dbname="{mysql database name}" ;
+$incidents="esp_incidents";
+$check_register="esp_check_register";    
+$sites_pay="esp_sites_pay";    
+$sites_techs="esp_sites_techs";
 
 $imap_server="{example.com:143}";
 $imap_user="email address for imap";
